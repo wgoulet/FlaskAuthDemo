893cf9d1dd276409ed19fd09024687cddd0c4fd1
Walter Goulet <wgoulet@gmail.com>
2019-10-26 03:03:18 +0000

    Completed login/logout flow and basic auth

--- flaskauthdemo.py
+++ flaskauthdemo.py
@@ -1,10 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, Response, redirect, url_for, request, session, abort, render_template
+from flask_login import LoginManager, UserMixin, \
+                                        login_required, login_user, logout_user 
+
+from user import FlaskDemoUser
+import os
+import re
+import sys
+import pprint
 
 app = Flask(__name__)
 
+app.config.update(
+    DEBUG = True,
+    SECRET_KEY = 'secret_xxx'
+)
+
+userdict = {}
+with open('./passwd.txt','r') as file:
+    for line in file:
+        uname,passwd = line.rstrip().split(":")
+        userdict[uname] = passwd
+
+pp = pprint.PrettyPrinter(stream=sys.stderr)
+pp.pprint(userdict)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if password == userdict[username]:
+            user = FlaskDemoUser(username)
+            login_user(user)
+            return redirect(request.args.get("next"))
+        else:
+            return abort(401)
+    else:
+        return Response('''
+        <form action="" method="post">
+        <p><input type=text name=username>
+        <p><input type=password name=password>
+        <p><input type=submit value=Login>
+        </form>
+        ''')
+        
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response("<p>Logged out</p>")
+
 @app.route("/")
+@login_required
 def home():
     return render_template("home.html")
 
+@login_manager.user_loader
+def load_user(userid):
+    return FlaskDemoUser(userid)
+
 if __name__=="__main__":
     app.run(host='0.0.0.0')
+
+
