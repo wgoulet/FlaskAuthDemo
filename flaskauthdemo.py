8e4658ad41493a3f3c899d4dcc0c6ae02df43f07
Walter Goulet <wgoulet@gmail.com>
2019-10-27 01:35:18 +0000

    Added serialization for user sessions and got OAuth flow working

--- flaskauthdemo.py
+++ flaskauthdemo.py
@@ -8,6 +8,8 @@ import re
 import sys
 import pprint
 import json
+import pickle
+from authlib.flask.client import OAuth
 
 app = Flask(__name__)
 
@@ -15,6 +17,7 @@ app.config.update(
     DEBUG = True,
     SECRET_KEY = 'secret_xxx'
 )
+
 
 userdict = {}
 with open('./passwd.txt','r') as file:
@@ -28,8 +31,25 @@ with open('./creds.txt','r') as file:
 pp = pprint.PrettyPrinter(stream=sys.stderr)
 pp.pprint(userdict)
 
-secret = creds['clientsecret']
-pp.pprint(secret)
+for key in creds:
+    pp.pprint(key)
+    pp.pprint(creds[key])
+
+oauth = OAuth(app)
+
+oauth.register(
+    name='AzureAD',
+    client_id=creds['clientid'],
+    client_secret=creds['clientsecret'],
+    access_token_url=creds['oauth2token'],
+    access_token_params=None,
+    authorize_url=creds['oauth2authz'],
+    authorize_params=None,
+    api_base_url=creds['apiendpoint'],
+    client_kwargs={'scope': 'openid'}
+)
+azured = oauth.create_client('AzureAD')
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -37,24 +57,27 @@ login_manager.login_view = "login"
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if password == userdict[username]:
-            user = FlaskDemoUser(username)
-            login_user(user)
-            return redirect(request.args.get("next"))
-        else:
-            return abort(401)
-    else:
-        return Response('''
-        <form action="" method="post">
-        <p><input type=text name=username>
-        <p><input type=password name=password>
-        <p><input type=submit value=Login>
-        </form>
-        ''')
-        
+    redirect_uri = url_for('authorize', _external=True)
+    pp.pprint(redirect_uri)
+    return azured.authorize_redirect(redirect_uri)
+
+@app.route('/authorize')
+def authorize():
+    token = azured.authorize_access_token()
+    pp.pprint(token) 
+    resp = azured.get('me')
+    profile = resp.json()
+    #pp.pprint(profile)
+    user = FlaskDemoUser(id=profile['id'])
+    user.name = profile['userPrincipalName']
+    pp.pprint(user.id)
+    pp.pprint(user.name)
+
+    pickle.dump(user,open("{0}.db".format(user.id),'wb'))
+    login_user(user)
+    # do something with the token and profile
+    return redirect('/')
+
 @app.route("/logout")
 @login_required
 def logout():
@@ -68,7 +91,13 @@ def home():
 
 @login_manager.user_loader
 def load_user(userid):
-    return FlaskDemoUser(userid)
+    u = FlaskDemoUser(userid)
+    pp.pprint("login manager callback")
+    pp.pprint(u.id)
+    pp.pprint(u.name)
+    user = pickle.load(open("{0}.db".format(userid),'rb'))
+    
+    return user
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
--- user.py
+++ user.py
@@ -3,6 +3,6 @@ from flask_login import LoginManager, UserMixin, \
                                         login_required, login_user, logout_user 
 
 class FlaskDemoUser(UserMixin):
-    def __init__(self,username):
-        self.name = username
-        self.id = username
+    name = None
+    def __init__(self,id):
+        self.id = id
